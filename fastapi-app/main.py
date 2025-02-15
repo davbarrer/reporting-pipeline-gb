7e96e113d18dd6dc0fa5b9316c2a2e4144837a32
root <root@PC-David>
2025-02-15 08:30:04 -0500

    Add env variables load
    *This adds the env variables from
    the .env file with the credentials

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI, HTTPException, Depends
 from models.request import InsertRequest
 from models.response import InsertResponse
 import asyncpg
-import os
 import traceback
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+env_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(env_path)
 
 app = FastAPI()
 
--- scripts/migration/migrate_csv_to_postgres.py
+++ scripts/migration/migrate_csv_to_postgres.py
@@ -5,6 +5,12 @@ import io
 import os
 import logging
 import watchtower
+from dotenv import load_dotenv
+import os
+
+env_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "..", ".env"))
+load_dotenv(env_path)
 
 # AWS Configuration
 S3_BUCKET = "data-pipeline-migration-gb"
