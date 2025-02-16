6253d9b0ac1407d8506868bb0e198fd38192ccff
root <root@PC-David>
2025-02-16 08:57:03 -0500

    clean unused models
    This was in case needed for further
    improvements, not needed for POC

--- fastapi-app/models/departments.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class DepartmentInsert(BaseModel):
-    id: int = Field(..., example=1)
-    department: str = Field(..., example="Engineering")
--- fastapi-app/models/jobs.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class JobInsert(BaseModel):
-    id: int = Field(..., example=1)
-    job: str = Field(..., example="Software Engineer")
